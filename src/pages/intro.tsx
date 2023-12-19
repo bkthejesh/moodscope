9adc9aa2f00411caa842b8ed5cc7e6ab37856c1b
Varsha1407 <kvarsha1407@gmail.com>
2023-12-19 17:01:59 +0530

    loading page

---
+++ src/pages/intro.tsx
@@ -0,0 +1,42 @@
+// Intro.tsx
+import { useRouter } from 'next/router';
+import Image from 'next/image';
+
+const Intro: React.FC = () => {
+  const router = useRouter();
+
+  const handleGetStartedClick = async () => {
+    // Redirect to the loading page
+    router.push('/loading');
+  };
+
+  return (
+    <div className="flex items-center justify-center h-screen bg-[#ff8473]">
+      <div className="text-center text-white">
+        <Image
+        src='/logo.png'
+        alt='Intro'
+        className="w-1/2 h-auto mb-4 mx-auto"
+        height={350}
+        width={350}
+        />
+    
+        
+        <h1 className="text-4xl mb-4 font-lucida-calligraphy">
+          MoodScope
+        </h1>
+        <p className="text-lg mb-6 font-lucida-calligraphy">
+          Welcome to MoodScope! Explore your emotions and enhance your well-being.
+        </p>
+        <button
+          onClick={handleGetStartedClick}
+          className="bg-blue-500 text-white py-2 px-4 rounded hover:bg-blue-600 font-lucida-calligraphy"
+        >
+          Get Started
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Intro;

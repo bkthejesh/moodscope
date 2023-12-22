535185a2ebc20a4a86d2ee6d1736f84b147c9b87
Varsha1407 <kvarsha1407@gmail.com>
2023-12-22 11:27:06 +0530

    changed main page

--- src/pages/mainpage.tsx
+++ src/pages/mainpage.tsx
@@ -1,69 +1,95 @@
 import React from 'react';
-
+import { Picker } from 'emoji-mart';
+import Image from 'next/image';
 
 const Mainpage = () => {
-    const gradientText = {
-        background: 'linear-gradient(to right, #F59115, #FFF878)',
-        WebkitBackgroundClip: 'text',
-        color: 'transparent',
-        marginBottom: '20px',
-        fontSize: '24px',
-        fontWeight: 'bold',
-      };
+  const gradientText = {
+    background: 'linear-gradient(to right, #F59115, #FFF878)',
+    WebkitBackgroundClip: 'text',
+    color: 'transparent',
+    marginBottom: '20px',
+    fontSize: '24px',
+    fontWeight: 'bold',
+  };
+
+  const emotionsWithEmojis = [
+    { emotion: 'Happy', emoji: '😊' },
+    { emotion: 'Sad', emoji: '😢' },
+    { emotion: 'Excited', emoji: '😄' },
+    { emotion: 'Worried', emoji: '😟' },
+    { emotion: 'Surprised', emoji: '😮' },
+    { emotion: 'Scared', emoji: '😨' },
+    { emotion: 'Crazy', emoji: '🤪' },
+    { emotion: 'Frustrated', emoji: '😤' },
+    { emotion: 'Angry', emoji: '😡' },
+    { emotion: 'Bored', emoji: '😐' },
+    { emotion: 'Sick', emoji: '🤢' },
+    { emotion: 'Embarrassed', emoji: '😳' },
+    { emotion: 'Tired', emoji: '😴' },
+    { emotion: 'Nervous', emoji: '😬' },
+    { emotion: 'Calm', emoji: '😌' },
+    { emotion: 'Confused', emoji: '😕' },
+    { emotion: 'Anxious', emoji: '😰' },
+    { emotion: 'Aggressive', emoji: '😠' },
+    { emotion: 'Sleepy', emoji: '😪' },
+    { emotion: 'Hungry', emoji: '🍔' },
+  ];
+
+  // Updated grid container style
+  const gridContainerStyle = {
+    display: 'grid',
+    gridTemplateRows: 'repeat(4, 1fr)',
+    gridTemplateColumns: 'repeat(5, 1fr)',
+    gap: '10px',
+    width: '100%', // Fill the width
+    height: 'auto', // Adjust height as needed
+    padding: '10px', // Optional padding
+  };
+
   return (
-    <div style={{
-      backgroundColor: '#1E1E1E',
-      backgroundImage: 'linear-gradient(#1E1E1E, #003041, #1E1E1E)',
-      height: '100vh',
-      display: 'flex',
-      flexDirection: 'column',
-      justifyContent: 'center',
-      alignItems: 'center',
-      border: '1px solid #1E1E1E',
-      boxSizing: 'border-box',
-      position: 'relative',
-    }}>
-      <div style={{ marginBottom: '20px' }}> {/* Add margin to create space between text and buttons */}
+    <div
+      style={{
+        backgroundColor: '#1E1E1E',
+        backgroundImage: 'linear-gradient(#1E1E1E, #003041, #1E1E1E)',
+        height: '100vh',
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems: 'center',
+        border: '1px solid #1E1E1E',
+        boxSizing: 'border-box',
+        position: 'relative',
+      }}
+    >
+      <div style={{ marginBottom: '20px' }}>
         <p style={gradientText}>Emotion for the day??</p>
       </div>
-      <div style={{    
-        display: 'grid',
-        gridTemplateRows: 'repeat(4, 1fr)',
-        gridTemplateColumns: 'repeat(5, 1fr)',
-        gap: '10px', 
-      }}>
-        <button style={buttonStyle}>Happy</button>
-        <button style={buttonStyle}>Sad</button>
-        <button style={buttonStyle}>Excited</button>
-        <button style={buttonStyle}>Worried</button>
-        <button style={buttonStyle}>Surprised</button>
-        <button style={buttonStyle}>Scared</button>
-        <button style={buttonStyle}>Crazy </button>
-        <button style={buttonStyle}>Frustrated</button>
-        <button style={buttonStyle}>Angry</button>
-        <button style={buttonStyle}>Bored</button>
-        <button style={buttonStyle}>Sick</button>
-        <button style={buttonStyle}>Embarrassed</button>
-        <button style={buttonStyle}>Tired</button>
-        <button style={buttonStyle}>Nervous</button>
-        <button style={buttonStyle}>Calm</button>
-        <button style={buttonStyle}>Confused</button>
-        <button style={buttonStyle}>Anxious</button>
-        <button style={buttonStyle}>Aggressive</button>
-        <button style={buttonStyle}>Sleepy</button>
-        <button style={buttonStyle}>Hungry</button>
+      <div style={gridContainerStyle}>
+        {emotionsWithEmojis.map(({ emotion, emoji }) => (
+        <div key={emotion}>
+          <button style={buttonStyle}>
+            <span style={{ fontSize: '40px', lineHeight: '60px' }}>{emoji}</span> {emotion}
+            </button>
+            </div>
+        ))}
       </div>
     </div>
   );
 };
 
 const buttonStyle = {
-  padding: '10px',
+  width: '100%', // Fixed width
+  height: '60px', // Fixed height
+  padding: '5px', // Reduced padding
   backgroundColor: '#D9D9D9',
   color: '#000000',
   border: 'none',
-  borderRadius: '20px',
+  borderRadius: '50px',
   cursor: 'pointer',
+  fontSize: '20px', // Adjust text size if needed
+  display: 'flex', // Enable flexbox
+  justifyContent: 'center', // Center horizontally
+  alignItems: 'center', // Center vertically
 };
 
 export default Mainpage;
